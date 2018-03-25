54bbbdca8c488e51f7639e86527347dd48ecc362
Victor Alves <vsilva.programador@gmail.com>
2018-03-25 19:04:57 -0300

    3rd Exercise

---
+++ ex3.rb
@@ -0,0 +1,22 @@
+# prints a string
+puts "I will now count my chickens:"
+# prints a string using interpolation
+puts "Hens #{25.0 + 30 / 6}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+# prints another string
+puts "Now I will count the eggs:"
+# prints the result of this equation
+puts 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# prints a boolean
+puts 3 - 2 < 5 - 7
+# prints another string with interpolation
+puts "What is 3 + 2? #{3.0 + 2}"
+puts "What is 5 - 7? #{5.0 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
