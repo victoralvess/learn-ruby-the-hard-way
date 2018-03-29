6d104b01c8517a66a0e754fa3c914d0b54f37f0f
Victor Alves <vsilva.programador@gmail.com>
2018-03-29 15:54:52 -0300

    Corrected 15th Ex.

--- ex15.rb
+++ ex15.rb
@@ -3,11 +3,15 @@ filename = ARGV.first
 txt = open(filename)
 
 puts "Here's your file #{filename}"
-print txt.read
+puts txt.read
+
+txt.close()
 
 print "Type the filename again: "
 print file_again = $stdin.gets.chomp
 
 txt_again = open(file_again)
 
-print txt_again.read
+puts txt_again.read
+
+txt_again.close()
